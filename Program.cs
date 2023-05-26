543b729c83da3a58697685b6f268bd6b2d166226
FazylovV <vladislav.fazylov04@gmail.com>
2023-05-26 18:36:57 +0500

    Init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace DummyDatabase
+{
+    class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
