6378271a1808f6de335bb80c7a8d339715d329e8
Fazylov <113243801+FazylovV@users.noreply.github.com>
2023-06-10 13:44:20 +0500

    Update Column.cs
    delete

--- Column.cs
+++ Column.cs
@@ -1,18 +1,1 @@
-namespace DummyDatabase;
 
-public class Column
-{
-    public string Name { get; }
-    public string Type { get; }
-
-    public Column(string name, string type)
-    {
-        Name = name;
-        Type = type;
-    }
-
-    public override string ToString()
-    {
-        return Name;
-    }
-}
